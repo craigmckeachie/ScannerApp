f79ad4a8501de2655f1573fc5caf3dc0a29df420
Craig McKeachie <craigdmckeachie@gmail.com>
2025-09-26 14:12:12 -0400

    scanner int example

--- src/main/java/com/pluralsight/ScannerExample.java
+++ src/main/java/com/pluralsight/ScannerExample.java
@@ -4,14 +4,25 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class ScannerExample {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("First Name: ");
         String firstName = scanner.nextLine();
-        scanner.nextLine();
 
+
+        System.out.println("Age: ");
+        int age = scanner.nextInt();
+
+        System.out.println();
         System.out.println("Hi " + firstName);
+        System.out.println("Your age is: " + age);
+
+
+
+
+
 
 
     }
